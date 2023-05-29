bb8b663f17f4fd2e46b5ca020e02ed2841e526b6
Obai Shaikh <obai.shaikh@gmail.com>
2023-05-29 18:47:23 +0300

    Update co2.py

--- co2.py
+++ co2.py
@@ -10,22 +10,21 @@ for f in fnames:
     df_list.append(pd.read_csv(f, index_col=False))
 
 df = pd.concat(df_list)
-df
+
 
 '---'
 import plotly.express as px
 
 fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", hover_name="City",
                         hover_data=["CO2 emission (Mton/yr)"],
-                        color="Sector", zoom=3, height=300)
+                        color="Sector", zoom=3, height=500)
 fig.update_layout(mapbox_style="open-street-map")
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 st.plotly_chart(fig)
 
-
-
 '---'
 
+df
 # fig = go.Figure(go.Scattermapbox(
 #     fill = "toself",
 #     lon = [-74, -70, -70, -74], lat = [47, 47, 45, 45],
