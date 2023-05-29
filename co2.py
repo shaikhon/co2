436bbcb925b1541d02427f6dfe8b412727ec8ffe
Obai Shaikh <obai.shaikh@gmail.com>
2023-05-29 19:41:58 +0300

    Update co2.py

--- co2.py
+++ co2.py
@@ -37,7 +37,8 @@ fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", hover_name="City",
                         category_orders=order,
                         color="Sector",
                         color_discrete_sequence=colors, zoom=4.2, width=800, height=600)
-fig.update_layout(mapbox_style="carto-positron")  #carto-positron
+fig.update_layout( title_text = "2022 Saudi Arabia's CO2 Emissions",
+                   mapbox_style="carto-positron")  #carto-positron
 fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 st.plotly_chart(fig)
 
