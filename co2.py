6e0c7f61eaf6cfb84c11d93631464ac00fc88c5b
Obai Shaikh <obai.shaikh@gmail.com>
2023-05-30 22:04:30 +0300

    co2.py

--- co2.py
+++ co2.py
@@ -15,11 +15,170 @@ st.set_page_config(
 )
 
 
+def annual_prophecy(d, ys, growth='linear', forecast_period=5):
+    '''
+    prophecy - FORECAST FUTURE STOCK PRICE
+
+    Inputs:
+    d                  Price history DataFrame (yfinance)
+    forecast_period    Number of minutes of future forecast to predict
+
+    '''
+    d.index.names = ['ds']  # rename index to ds
+    d = d.tz_localize(None)  # make timezone naive, for prophet
+    ds = d.reset_index()  # make index (ds) a column
+
+    for y in ys:
+        ds_temp = ds.loc[:, ['ds', y]].rename(columns={y: 'y'})
+
+        # Make the prophet model and fit on the data
+        gm_prophet = Prophet(
+            growth=growth,
+            changepoints=None,
+            n_changepoints=len(ds) // 4,
+            changepoint_range=.99,
+            yearly_seasonality=True,
+            weekly_seasonality=False,
+            daily_seasonality=False,
+            holidays=None,
+            seasonality_mode='additive',
+            seasonality_prior_scale=1,  # yhat zigzag
+            holidays_prior_scale=0,
+            changepoint_prior_scale=.1,  # yhat slope
+            mcmc_samples=0,
+            interval_width=0.80,
+            uncertainty_samples=1000,
+            stan_backend=None
+        )
+
+        gm_prophet.fit(ds_temp)
+        # predict:
+        # Make a future dataframe
+        gm_forecast = gm_prophet.make_future_dataframe(periods=forecast_period, freq='Y')
+        # Make predictions
+        gm_forecast = gm_prophet.predict(gm_forecast)
+        # gm_forecast
+        gm_forecast = gm_forecast.set_index(gm_forecast.ds).loc[:, ['yhat', 'yhat_lower', 'yhat_upper',
+                                                                    'trend', 'trend_lower', 'trend_upper']]
+        # merge
+        d = gm_forecast.merge(d, how='outer', on='ds')
+
+    return d
+
+
+def prophet(d):
+    color = 'lime'  # if current_price >= open else 'rgb(255, 49, 49)'
+    x = d.index.to_list()
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+
+    # # plot price
+    # fig.add_trace(go.Scatter(mode="lines", x=x, y=d["Close"],
+    #                          line={"color": color,  # limegreen, lime, #E1FF00, #ccff00
+    #                                "width": 2,
+    #                                },
+    #                          hovertemplate='<i>Price</i>: $%{y:.2f}'
+    #                                        + '<br><i>Time</i>: %{x| %H:%M}'
+    #                                        + '<br><i>Date</i>: %{x|%a, %d %b %y}<extra></extra>',
+    #                          ),
+    #               secondary_y=True)
+
+    # plot yhat (CO2)
+    fig.add_trace(go.Scatter(mode='lines', x=x, y=d.yhat_y,
+                             # line=dict(color='rgba(255,255,255,1)', width=1),
+                             line=dict(color=color, width=1),
+                             hovertemplate='<i>Forecast</i>: $%{y:.2f}' +
+                                           '<br><i>Time</i>: %{x|%H:%M}<br><extra></extra>',
+                             showlegend=False),
+                  secondary_y=False)
+
+    # plot pop
+    fig.add_trace(go.Scatter(mode='lines', x=x, y=d.yhat_x / 1e6,
+                             line=dict(color='magenta', width=1),
+                             hovertemplate='<i>Forecast</i>: $%{y:.2f}' +
+                                           '<br><i>Year</i>: %{x|%Y:%H:%M}<br><extra></extra>',
+                             showlegend=False),
+                  secondary_y=True)
+
+    #     # plot volume bars
+    #     fig.add_trace(go.Bar(x=x, y=d["pop"]/1e6, opacity=.65,
+    #                          marker={
+    #                              "color": "magenta",  # "#0FCFFF"
+    #                          },
+    #                          hovertemplate='<i>population</i>: %{y:,}<extra></extra>'
+    #                          ), secondary_y=True)
+
+    #     # plot trend error bands
+    #     upper = d.trend_upper.to_list()
+    #     lower = d.trend_lower.to_list()
+
+    #     fig.add_trace(go.Scatter(x=x + x[::-1],
+    #                              y=upper + lower[::-1],
+    #                              fill='toself',
+    #                              fillcolor='rgba(255,255,255,.25)',
+    #                              line=dict(color='rgba(255,255,255,1)'),
+    #                              hoverinfo='skip',
+    #                              showlegend=False),
+    #                   secondary_y=True)
+
+    #     # plot price
+    #     fig.add_trace(go.Scatter(mode="lines", x=x, y=d['co2_mt'],
+    #                              line={"color": color,  # limegreen, lime, #E1FF00, #ccff00
+    #                                    "width": 2,
+    #                                    },
+    #                              hovertemplate='<i>CO2</i>: $%{y:.2f}'
+    #                                            + '<br><i>Time</i>: %{x| %H:%M}'
+    #                                            + '<br><i>Date</i>: %{x|%a, %d %b %y}<extra></extra>',
+    #                              ),
+    #                   secondary_y=True)
+
+    fig.update_layout(
+        hovermode="closest",
+        hoverlabel=dict(align="left", bgcolor="rgba(0,0,0,0)"),
+        #         template="plotly_dark",
+        margin=dict(t=10, b=10, l=10, r=10),
+        paper_bgcolor="rgba(0,0,0,0)",
+        plot_bgcolor="rgba(0,0,0,0)",
+        showlegend=False,
+        yaxis=dict(showgrid=False, title={"font": dict(size=24), "text": "CO2 (mt)", "standoff": 10}),
+        yaxis2=dict(showgrid=False, title={"font": dict(size=24), "text": "Population (million)", "standoff": 10}),
+        xaxis=dict(showline=False)
+    )
+    return fig
+
+
+def co2_ml():
+    fnames = glob.glob('./co2/data/*.csv')
+
+    # kt of co2
+    df_list = []
+    for f in fnames:
+        df = pd.read_csv(f).T.dropna()
+        df_list.append(df)
+
+    df = pd.concat(df_list, axis=1)
+    df.rename(columns={0: 'co2_kt', 1: 'pop'}, inplace=True)
+    df['co2_mt'] = df.loc[:, 'co2_kt'] / 1000
+    df.index = pd.to_datetime(df.index)
+
+    cols = ['co2_mt', 'pop']
+
+    df = annual_prophecy(df, cols, forecast_period=15)
+
+    fig = prophet(df)
+
+    return fig
+
+####################################################################################
+####################################################################################
+####################################################################################
+####################################################################################
+####################################################################################
+
 st.title('Green Guardians :four_leaf_clover:')
 
 
 # Read files
-fnames = glob.glob('./data/*.csv')
+fnames = glob.glob('./data/by_sector/*.csv')
 
 df_list = []
 for f in fnames:
@@ -42,13 +201,17 @@ fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", hover_name="City",
                         category_orders=order,
                         color="Sector",
                         color_discrete_sequence=colors, zoom=3, width=450, height=350)
-fig.update_layout( title_text = "2022 Saudi Arabia's CO2 Emissions",
+fig.update_layout( title_text = "2020 Saudi Arabia's CO2 Emissions",
                    mapbox_style="carto-positron")  #carto-positron
 fig.update_layout(margin={"r":0,"t":30,"l":0,"b":0})
-st.plotly_chart(fig)
+st.plotly_chart(fig)   # USE COLUMN WIDTH OF CONTAINER
 
 '---'
+# CO2 ML prediction
+st.plotly_chart(co2_ml())   # USE COLUMN WIDTH OF CONTAINER
 
+
+'---'
 df
 # fig = go.Figure(go.Scattermapbox(
 #     fill = "toself",
