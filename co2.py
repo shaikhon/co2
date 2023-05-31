cdc36028887810cb6c8bcbb7f3713c49e9720a4d
Obai Shaikh <obai.shaikh@gmail.com>
2023-05-31 17:15:13 +0300

    Update co2.py

--- co2.py
+++ co2.py
@@ -72,7 +72,7 @@ def annual_prophecy(d, ys, growth='linear', forecast_period=5):
             growth=growth,
             changepoints=None,
             n_changepoints=len(ds),
-            changepoint_range=.94,
+            changepoint_range=.92,
             yearly_seasonality=True,
             weekly_seasonality=False,
             daily_seasonality=False,
