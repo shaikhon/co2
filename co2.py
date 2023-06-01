7accc5616f9a6bfe7de8157be12ffcb69eb263f1
Obai Shaikh <obai.shaikh@gmail.com>
2023-06-01 09:49:26 +0300

    Update co2.py

--- co2.py
+++ co2.py
@@ -264,9 +264,8 @@ color_by = cols[2].selectbox('Color by:', ['Sector', 'Province', 'Primary Fuel',
 # with st.container():
 #     st.plotly_chart(co2_map(color_by), use_container_width=True)   # USE COLUMN WIDTH OF CONTAINER
 
-tab1 = st.tabs(["CO2 Emissions Map - 2020"])
 
-with tab1:
+with st.expander("CO2 Emissions Map - 2020"):
     st.plotly_chart(co2_map(color_by), use_container_width=True)   # USE COLUMN WIDTH OF CONTAINER
 
 '---'
