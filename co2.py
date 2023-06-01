54ffe72d349da117d7d833da8bd9c49992dba572
Obai Shaikh <obai.shaikh@gmail.com>
2023-06-01 10:06:55 +0300

    Update co2.py

--- co2.py
+++ co2.py
@@ -79,7 +79,7 @@ def annual_prophecy(d, ys, growth='linear', forecast_period=15):
             daily_seasonality=False,
             holidays=None,
             seasonality_mode='additive',
-            seasonality_prior_scale= 100, #0.05,  # yhat zigzag
+            seasonality_prior_scale= 0.01, #0.05,  # yhat zigzag
             holidays_prior_scale=0,
             changepoint_prior_scale=.25, #  .25,  # yhat slope, largefr == steeper
             mcmc_samples=0,
