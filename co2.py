869098b150b2e78073f43dad58e81b8ea423eec7
Obai Shaikh <obai.shaikh@gmail.com>
2023-06-05 19:40:27 +0300

    Update co2.py

--- co2.py
+++ co2.py
@@ -172,13 +172,13 @@ def prophet_plot(d):
 
     fig.add_hline(y=278, line_width=3, line_dash="dash", line_color="yellow", annotation_text="2030 278MM Goal",
                   annotation_position="top left",
-                  annotation_font_size=16,
+                  annotation_font_size=14,
                   )
 
     fig.add_hline(y=d.yhat_y.iloc[-1], line_width=3, line_dash="dash", line_color="cyan",
                   annotation_text="2060 Net-zero Goal",
                   annotation_position="top left",
-                  annotation_font_size=16,
+                  annotation_font_size=14,
                   )
 
     fig.update_layout(
